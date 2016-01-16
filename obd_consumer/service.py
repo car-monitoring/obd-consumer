af0cc50984f26a1a4cbdef68984e79cc07bc8d48
Michal Rostecki <mrostecki@mirantis.com>
2016-01-16 14:32:27 +0100

    Send statistics to server

--- obd_consumer/service.py
+++ obd_consumer/service.py
@@ -9,6 +9,7 @@ import functools
 import time
 
 import obd
+import requests
 import six
 
 from obd_consumer import cache
@@ -16,8 +17,19 @@ from obd_consumer import cache
 
 MAX_RETRY_COUNT = 5
 TIME_INTERVAL = 5
+STATS_FREQUENCY = 6
 
 _CONN = None
+
+STATS_SUFIX_MAP = {
+    "0104: Calculated Engine Load": "/metrics/api/engine_load",
+    "012F: Fuel Level Input": "/metrics/api/fuel_level",
+    "0103: Fuel System Status": "/metrics/api/fuel_status"
+    "0151: Fuel Type": "/metrics/api/fuel_type",
+    "010B: Intake Manifold Pressure": "/metrics/api/intake_pressure",
+    "010C: Engine RPM": "/metrics/api/rpm/",
+    "010D: Vehicle Speed": "/metrics/api/speed/"
+}
 
 
 def get_connection():
@@ -54,13 +66,43 @@ def fetch_data(command):
     return None, None
 
 
+def push_to_server(command, value, unit):
+    if command not in STATS_SUFIX_MAP:
+        return
+
+    config = six.moves.configparser.ConfigParser()
+    config.read('/etc/obd-consumer.conf')
+
+    url = config['DEFAULT']['url']
+    username = config['DEFAULT']['username']
+    password = config['DEFAULT']['password']
+
+    payload = {"username": username, "password": password}
+
+    response = requests.post(url + '/api-token-auth', data=payload)
+
+    if not response.ok:
+        return
+
+    token = response.json()['token']
+    header = {'Authorization': 'Token ' + token}
+    data = {"value": value, "unit": unit}
+    sufix = STATS_SUFIX_MAP[command]
+    requests.post(url + sufix, data=data, headers=header)
+
+
 def consumer_service():
+    x = 0
     while True:
         connection = get_connection()
         for command in connection.supported_commands:
             value, unit = fetch_data(command)
             cache.push_data(command, value, unit)
             print (command)
+
+            if x == STATS_FREQUENCY:
+                push_to_server(command, value, unit)
+                x = 0
 
 
         time.sleep(TIME_INTERVAL)
